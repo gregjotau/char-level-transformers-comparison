d66b8df5550cd4bce1ca3eeb01897722a3a89bbd
GT <gregjotau@gmail.com>
2025-01-22 15:26:58 +0100

    fix

--- copy-to-server.sh
+++ copy-to-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Define the server details
-SERVER="ec-gregorjt@fox.educloud.no"
 
 # Copy the files
-scp sanity.py sample.slurm train.py ${SERVER}:~/
+scp sanity.py sample.slurm train.py edu:~/
